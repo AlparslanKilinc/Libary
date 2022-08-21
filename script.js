734bc98b1f8cd9e34f79d8ba8941968e5a79c599
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-08-21 17:27:19 -0400

    Add books to content area.
      Created functions to collect data from form.
      using the information from the form created book objects.
      using book objects made content box's.
      then push them to content area.

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ class Library{
         this.library.pop(book);
     }
 
+    getArray(){
+        return this.library;
+    }
+
     getBook(title){
         return this.library.find( (book) => book.title==title)
     }
@@ -26,12 +30,68 @@ class Book {
     }
 }
 
+let library= new Library();
+let content_box=[];
 /// Document Elements
 let form = document.getElementById("book-form");
+let content= document.getElementById("content");
+
+
 
 
 /// Form
 let showForm= () => {form.style.display='flex';}
-let closeForm = ()=>{form.style.display='none';}
+let closeForm= () => {form.style.display='none';}
+
+let collectData = (event) =>{
+    event.preventDefault();
+    closeForm();
+    // get book information from form
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read= document.getElementById("read").checked;
+    /// create book and add it to library
+    let book = new Book(title,author,pages,read);
+    library.addBook(book);
+    addToContent();
+    console.log("12");
+}
+
+
+let create_book_box = (book) =>{
+    let box = document.createElement("div");
+    let title= document.createElement("p");
+    let author= document.createElement("p");
+    let pages= document.createElement("p");
+    let read= document.createElement("p");
+    
+    title.textContent=book.title;
+    author.textContent=book.author;
+    pages.textContent=book.pages;
+    if(book.read) read.textContent="Completed";
+    else read.textContent="In Progress";
+
+    box.appendChild(title);
+    box.appendChild(author);
+    box.appendChild(pages);
+    box.appendChild(read);
+    
+    box.classList.add("book-box");
+    return box;
+}
+
+/// Adding Books to Content
+let addToContent = () =>{
+
+    for(let book of library.getArray()){
+        if (content_box.includes(book.title)) continue;
+       content_box.push(book.title);
+       let book_box = create_book_box(book);
+       content.appendChild(book_box);
+    }
+
+}
+
 
 
