eba617a2a4a70195759bab35fc455812c25bfd01
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-08-24 11:01:08 -0400

    Add Read Button and Style Content Box
        added a read button that changes from
        complete to In Progress and styled the
        content box.

--- script.js
+++ script.js
@@ -42,6 +42,8 @@ let content= document.getElementById("content");
 let showForm= () => {form.style.display='flex';}
 let closeForm= () => {form.style.display='none';}
 
+
+/// Collect Data
 let collectData = (event) =>{
     event.preventDefault();
     closeForm();
@@ -56,34 +58,6 @@ let collectData = (event) =>{
     addToContent();
 }
 
-
-
-let create_book_box = (book) =>{
-    let box = document.createElement("div");
-    let title= document.createElement("p");
-    let author= document.createElement("p");
-    let pages= document.createElement("p");
-    let read= document.createElement("p");
-    let delete_btn=document.createElement("button");
-    delete_btn.textContent="Remove";  
-    delete_btn.addEventListener("click", () => remove_Content(book)); 
-   
-    title.textContent= "Title:"+" "+book.title;
-    author.textContent="Author:"+" "+book.author;
-    pages.textContent="Page Amount:"+" "+book.pages;
-    if(book.read) read.textContent="Completed";
-    else read.textContent="In Progress";
-  
-    box.appendChild(title);
-    box.appendChild(author);
-    box.appendChild(pages);
-    box.appendChild(read);
-    box.appendChild(delete_btn);
-    
-    box.classList.add("book-box");
-    return box;
-}
-
 /// Adding Books to Content
 let addToContent = () =>{
     content.innerHTML="";
@@ -96,12 +70,76 @@ let addToContent = () =>{
     }
 }
 
+
+
+let create_book_box = (book) =>{
+    let box = document.createElement("div");
+    let title= document.createElement("p");
+    let author= document.createElement("p");
+    let pages= document.createElement("p");
+    let read_btn = document.createElement("button");
+    let delete_btn=document.createElement("button");
+
+
+    /// Delete
+    delete_btn.textContent="Remove";  
+    delete_btn.addEventListener("click", () => remove_Content(book)); 
+    delete_btn.classList.add("delete_btn");
+
+    /// Read Button
+    read_btn.classList.add("read_btn");
+    read_btn.addEventListener("click",() => update_read(book,read_btn));
+    if(book.read){
+        read_btn.textContent="Completed";
+        read_btn.style.backgroundColor="#1DB954";
+    } 
+    else{
+        read_btn.textContent="In Progress";
+        read_btn.style.backgroundColor="#D22B2B";
+    }
+
+   
+    title.textContent= "Title:"+" "+book.title;
+    author.textContent="Author:"+" "+book.author;
+    pages.textContent="Page Amount:"+" "+book.pages;
+    
+
+    box.appendChild(title);
+    box.appendChild(author);
+    box.appendChild(pages);
+    box.appendChild(read_btn);
+    box.appendChild(delete_btn);
+    
+    box.classList.add("book-box");
+    return box;
+}
+
+
+
 let remove_Content = (book) =>{
     let index = content_box.indexOf(book.title);
     content_box.splice(index,1);
     library.removeBook(book);
     addToContent();
 }
+
+let update_read = (book,read_btn) => {
+    book.read=!book.read;
+    read=book.read;
+    if(read){ 
+        read_btn.style.backgroundColor="#1DB954";
+        read_btn.textContent="Completed";
+    } 
+    else{
+        read_btn.textContent="In Progress";
+        read_btn.style.backgroundColor="#D22B2B";
+    }
+}
+
+/// Place Holder Content: 
+let dummy_book= new Book("Harry Potter and the Philospher's Stone","J.K. Rowling",223,true);
+library.addBook(dummy_book);
+addToContent();
 
 
 
